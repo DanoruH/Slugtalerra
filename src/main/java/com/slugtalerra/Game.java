39270864be127e12e37de7e5fb1bfeb13c753c71
Danoru <berppydan@gmail.com>
2026-03-21 21:28:44 -0500

    new files

---
+++ src/main/java/com/slugtalerra/Game.java
@@ -0,0 +1,26 @@
+package com.slugtalerra;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
+ * event listeners.
+ */
+public class Game extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public Game(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.getName());
+    }
+}
